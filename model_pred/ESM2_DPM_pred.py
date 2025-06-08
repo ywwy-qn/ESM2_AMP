78e7e94581a3cb16918d4668acf7fc834d4fdf78
ywwy-qn <2108437154@qq.com>
2025-06-08 12:59:10 +0800

    Add files via upload

---
+++ model_pred/ESM2_DPM_pred.py
@@ -0,0 +1,58 @@
+import os
+
+import torch
+import torch.nn as nn
+
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent
+sys.path.append(str(project_root))
+
+# 导入模块
+from AMPmodel.dataset import load_dataset
+from AMPmodel.model import DNN
+from AMPmodel.check import fix_state_dict, evaluate_model
+
+# 设备设置
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+# 构建正确的文件路径
+feature_file = os.path.join(project_root, "model_pred/data", "real_test_dataset_features.h5")
+sample_file = os.path.join(project_root, "model_pred/data", "real_test_dataset_samples.xlsx")
+
+test_dataset = load_dataset(
+    mode='mean',
+    feature_file=feature_file,  # 使用绝对路径
+    sample_file=sample_file
+)
+
+
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+# 加载模型
+model = DNN(input_dim=2560, hidden1_dim=1280, hidden2_dim=480, hidden3_dim=100, output_dim=1).float().to(device)
+checkpoint = torch.load("model_pred/weights_file/ESM2_DPM.pth")
+
+if 'module' in list(checkpoint['model_state_dict'].keys())[0]:
+    checkpoint['model_state_dict'] = fix_state_dict(checkpoint['model_state_dict'])
+
+model.load_state_dict(checkpoint['model_state_dict'])
+
+# 评估模型
+criterion = nn.BCEWithLogitsLoss()
+avg_loss, accuracy, recall, f1, auc, mcc, precision, predictions_df = evaluate_model(model, test_dataloader, criterion, device)
+
+# 输出结果
+# print(f"avg_loss: {avg_loss:.4f}")
+print(f"accuracy: {accuracy:.4f}")
+print(f"recall: {recall:.4f}")
+print(f"f1 score: {f1:.4f}")
+print(f"AUC: {auc:.4f}")
+print(f"MCC: {mcc:.4f}")
+print(f"Precision: {precision:.4f}")
+
+# 保存预测结果
+predictions_df.to_csv(os.path.join('test_predictions_ESM2_DPM.csv'), index=False)
